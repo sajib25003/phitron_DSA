c4216f79881b5b8752cf16df996da9474dd99ca1
Md. Ashik Hassan Bhuiyan <sjb.25003@gmail.com>
2026-07-12 20:10:18 +0600

    fibonacci bottom up - practice

--- Algorithm/Module-14/fibonacci_with_memoization.cpp
+++ Algorithm/Module-14/fibonacci_with_memoization.cpp
@@ -1,17 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dp[1005];
 int fibo(int n)
 {
     if (n < 2)
         return n;
-    return fibo(n - 1) + fibo(n - 2);
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+    dp[n] = fibo(n - 1) + fibo(n - 2);
+    return dp[n];
 }
 
 int main()
 {
+    memset(dp, -1, sizeof(dp));
     int n;
     cin >> n;
-    cout << fibo(n);
+    for (int i = 0; i <= n; i++)
+    {
+        cout << i << " -> " << fibo(i) << endl;
+    }
     return 0;
 }
---
+++ Algorithm/Module-14/fobonacchi_bottom_up.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int fibo[n + 1];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        cout << i << " -> " << fibo[i] << endl;
+    }
+    return 0;
+}
